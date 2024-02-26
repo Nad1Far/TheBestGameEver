b77be21975e1b59ef7aa096e5c3104e0dde9e9f3
Nad1Far <147243971+Nad1Far@users.noreply.github.com>
2024-02-27 00:11:06 +0500

    Fireballs

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value = 100;
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10;
+
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+
+
+    public void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+
+    public void DamageEnemy(Collision collision)
+    {
+        var EnemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (EnemyHealth != null)
+        {
+            EnemyHealth.DealDamage(damage);
+        }
+    }
+
+}
